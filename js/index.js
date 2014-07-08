fe38e70111218f13fac9986fbd2e0e25a5dc26fe
Rhadow <HowardChang@HowardsMacBook-Pro.local>
2014-07-08 22:27:53 +0800

    bottleneck on angular

--- js/index.js
+++ js/index.js
@@ -7,6 +7,19 @@ var app = angular.module('myResume', []);
 
 app.controller('navCtrl', ['$scope',
     function($scope) {
+        $scope.navHeaders = [{
+            name: "Intro",
+            active: false
+        }, {
+            name: "Skills",
+            active: false
+        }, {
+            name: "Contact Information",
+            active: false
+        }, ];
 
+        $scope.clickHandler = function(index) {
+            $scope.navHeaders[index].active = true;
+        }
     }
-]);
+])
