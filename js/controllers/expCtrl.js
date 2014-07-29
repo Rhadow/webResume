3354aa858d73079b8b01c89c580cd3c7bdfce0ac
Rhadow <howard3433@gmail.com>
2014-07-29 23:53:46 +0800

    Carousel added, style still needs to be worked on

--- js/controllers/expCtrl.js
+++ js/controllers/expCtrl.js
@@ -2,6 +2,6 @@ angular.module("app.controllers").controller("expCtrl", ["$scope", "Experiences"
     function($scope, Experiences) {
         Experiences.fetch().then(function(data) {
             $scope.data = data;
-        })
+        });
     }
 ]);
